5eb348e45493676b32c3e4bff933e79292a9646b
Alex Hagopian <hagopianalexander@gmail.com>
2024-12-26 18:36:23 -0500

    Generate data.

--- src/data_generation.py
+++ src/data_generation.py
@@ -1,7 +1,8 @@
+import os
 import pandas as pd
 import numpy as np
 
-def generate_sin_data(num_samples=10000, output_file="data/sin_data.csv"):
+def generate_sin_data(num_samples=10000, output_file="../data/sin_data.csv"):
     """
     Generates a dataset for sin(x) and saves it to a CSV file.
 
@@ -9,12 +10,12 @@ def generate_sin_data(num_samples=10000, output_file="data/sin_data.csv"):
         num_samples (int): Number of data points to generate.
         output_file (string): Path to the output file.
     """
-    x = np.random.uniform(low=-np.pi, high=(2*np.pi, num_samples))
+    x = np.random.uniform(low=-np.pi, high=(2*np.pi), size=num_samples)
     y = np.sin(x)
 
     data = pd.DataFrame({'x': x, 'y': y})
     data.to_csv(output_file, index=False)
 
-
+generate_sin_data()
 
     
